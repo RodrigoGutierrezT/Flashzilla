135d852e90a9b2ae178f529746b455798379c1c5
RodrigoGutierrezT <rodrigo644@gmail.com>
2024-09-04 21:08:40 -0400

    Initial commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Rodrigo on 04-09-24.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
