0ee8b1a8a872af566211b44b9774c92e09c29441
RodrigoGutierrezT <rodrigo644@gmail.com>
2024-09-06 20:05:23 -0400

    feat: Add Card struct

---
+++ Flashzilla/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Rodrigo on 06-09-24.
+//
+
+import Foundation
+
+struct Card {
+    var prompt: String
+    var answer: String
+    
+    static let example = Card(prompt: "Who played the 13th Doctor in Doctor Who?", answer: "Jodie Whittaker")
+}
